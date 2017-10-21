df6a80b54c8e6fb868263d63b012ccb6700ef963
Dave Smith <Dave.M.Smith@accenture.com>
2017-10-21 18:18:35 +0100

    RDK-17791: Update copyright headers.
    Reason for change: Update headers to Apache-2.0
    Test Procedure: Build only.
    Risks: None
    Signed-off-by: Dave Smith <dave.m.smith@accenture.com>
    
    Change-Id: I7076f2c9650d1e83149580cc589139e8241c4d1f

--- pxDiagnostics/modules/color.js
+++ pxDiagnostics/modules/color.js
@@ -1,4 +1,21 @@
-
+/*
+ * If not stated otherwise in this file or this component's Licenses.txt file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2016 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
 var Color = {RED:0xff0000ff, GREEN:0x00ff00ff, BLUE:0x0000ffff, CYAN:0x00ffffff,
   X1_BLUE:0x0085cbff, DARKBLUE:0x33505eff, ALMOST_WHITE:0xffffffe5,
   GREY:0xffffffb7, DARKGREY:0xffffff73, STROKEGREY:0xffffff35,
